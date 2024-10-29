0c2f389bf39725d0c2076aa9456d2f387d52dbae
ShekharGlobal <traininglabit@gmail.com>
2024-10-29 15:44:20 +0530

    Add files via upload

---
+++ Core Java/29th October 2024/Solutions/Statement1/Triangle.java
@@ -0,0 +1,15 @@
+// Triangle.java
+package com.coforge.statement1;
+
+public class Triangle {
+    int sides;
+
+    public Triangle(int sides) {
+        this.sides = sides;
+    }
+
+    public void calculateArea() {
+        double area = 0.433 * sides * sides;
+        System.out.println("The Area of the Triangle is " + area);
+    }
+}
