dc487d3c749546a26b1b6c9191975e6598314d11
ShekharGlobal <traininglabit@gmail.com>
2024-11-15 08:32:06 +0530

    Add files via upload

---
+++ Spring Framework/Spring Core/Examples/Aggregation/Address.java
@@ -0,0 +1,11 @@
+package com.coforge.aggregation;
+
+public class Address {
+
+	String city, state, country;
+
+	
+	  public Address(String city, String state, String country) { super();
+	  this.city = city; this.state = state; this.country = country; }
+	 
+}
