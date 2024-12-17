59bedda3b260be1022df361d1e66daeb91412f29
ShekharGlobal <traininglabit@gmail.com>
2024-12-17 08:54:28 +0530

    Add files via upload

---
+++ Web Development/Servlet/SendRedirect/Servlet2.java
@@ -0,0 +1,37 @@
+package com.cdac.ServletEx;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+@WebServlet("/Servlet2")
+public class Servlet2 extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Set response content type
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        // Retrieve the attribute from the forwarded request
+        String message = (String) request.getAttribute("message");
+
+        // Print the message to the browser
+        out.print("<html><body>");
+        out.print("<h1>" + message + "</h1>");
+        out.print("</body></html>");
+    }
+}
+
