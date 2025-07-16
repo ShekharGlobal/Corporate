0bd311b59bd1c1bb348931627321c3e6338c4b09
ShekharGlobal <traininglabit@gmail.com>
2025-07-16 20:54:59 +0530

    Add files via upload

---
+++ Hibernate/Hibernate/Main.java
@@ -0,0 +1,29 @@
+package com.hcl.orm.crud;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CRUD crud = new CRUD();
+
+        // Insert Operation
+        crud.insertEmployee(145, "Vijay", "R");
+
+        // Select Operation
+        //crud.selectEmployee(145);
+
+        // Update Operation
+        //crud.updateEmployee(145, "Kumar");
+
+        // Select to verify update
+       // crud.selectEmployee(145);
+
+        // Delete Operation
+       // crud.deleteEmployee(145);
+
+        // Select to verify delete
+      //  crud.selectEmployee(145);
+
+        // Close the factory after all operations
+        CRUD.closeFactory();
+    }
+}
